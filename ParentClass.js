3723ab63aa74c450e393d4205caf6206c3ab0c06
Manas-k <74486519+Manas-k@users.noreply.github.com>
2021-02-27 13:45:52 +1100

    Add files via upload

---
+++ ParentClass.js
@@ -0,0 +1,18 @@
+class ParentClass{
+    constructor(x, y, width, height) {
+        var options = {
+            'restitution':0.4,
+            'friction':0.0,
+        }
+        this.body = Bodies.rectangle(x, y, width, height, options);
+        this.width = width;
+        this.height = height;
+        World.add(world, this.body);
+      }
+      display(){
+        push();
+        translate(this.body.position.x, this.body.position.y);
+        rectMode(CENTER);
+        pop();
+      }
+}
